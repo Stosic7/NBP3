5e25625f6ab1d74a98080fb7c985f197e04d9c61
Luka Stošić <lukastosic22@gmail.com>
2026-03-05 00:42:54 +0100

    final

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,16 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Atlas connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      family: 4
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
 };
